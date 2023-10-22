d6e304831bd9e9f9aa297e57886b8fc527ad826e
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-10-22 19:54:38 +0400

    add LocalStorage

--- js/knockout.js
+++ js/knockout.js
@@ -1,12 +1,12 @@
-function todoModel(todoText){
+function todoModel(todoText,CompleteStatus){
     var self=this;
     self.todoText=todoText;
     self.isActive = ko.observable(true);
-    self.isComplete = ko.observable(false);
+    self.isComplete = ko.observable(CompleteStatus);
     self.isVisible= ko.observable(true);
 }
-const inputTodoText=document.querySelector('.inputTodoText');
 
+const inputTodoText=document.querySelector('.inputTodoText');
 function todoappViewModel(){
     var self=this;
     self.menus=['All','Active','Completed'];
@@ -15,10 +15,14 @@ function todoappViewModel(){
     self.todoList=ko.observableArray([]);
     self.addFunction=function(){
         if(inputTodoText.value){
-            self.todoList.push(new todoModel(inputTodoText.value));
+            self.todoList.push(new todoModel(inputTodoText.value,false));
             inputTodoText.value='';
         }
+        for (let i = 0; i < (this.todoList()).length; i++) {
+            addToLocalStorageArray(this.todoList()[i].todoText,this.todoList()[i].isComplete())
+        }
     }
+    
     self.inputVisible=ko.observable(true);
     self.goToMenu = function(menu){
         
@@ -43,20 +47,74 @@ function todoappViewModel(){
         }
     }
     self.goToMenu('All');
+
     self.deleteTodo=function(todo){
+        localStorage.removeItem(todo.todoText);
         self.todoList.remove(todo);
     }
-    self.editable=function(){
+    self.editable=function(todo){
         this.isActive(false);
+        localStorage.removeItem(todo.todoText);
     }
-    self.saveItem=function(){
+    self.saveItem=function(todo){
         this.isActive(true);
+        localStorage.setItem(todo.todoText,todo.isComplete())
     }
     self.makeComplete=function(){
         this.isComplete(true);
+        self.todoList().forEach(function (todo) {
+            if(todo.isComplete()==true){
+                localStorage.setItem(todo.todoText,todo.isComplete())
+            };
+        });
     }
+    self.Reload =document.addEventListener('DOMContentLoaded', function(e){
+        e.preventDefault();
+        let tasks=[];
+        for (i = 0; i < localStorage.length; i++) {
+            let task=localStorage.getItem(localStorage.key(i));
+            tasks.push(localStorage.key(i),task);
+            self.todoList.push(new todoModel(localStorage.key(i),JSON.parse(tasks[1])))
+            tasks.pop(localStorage.key(i))
+            tasks.pop(task)
+        }
+    })
+    self.uploadfunction=function(){
+        localStorage.clear();
+        const request = new XMLHttpRequest();
+        request.open('GET', 'file/data.json');
+        request.send();
+        request.onload = function () {
+            var items = JSON.parse(request.response)
+            console.log(items.length)
+            for(let i=0;i<items.length;i++){
+                localStorage.setItem(items[i].todo,items[i].completed)
+                self.todoList.push(new todoModel(localStorage.key(i),localStorage.getItem(localStorage.key(i))))
+            }
+        }
 
-    
+    }
+    self.downloadfunction=function(){
+        
+    }
 };
 
 ko.applyBindings(new todoappViewModel());
+
+var addToLocalStorageArray = function (todo, value) {
+    
+    var tasks;
+	
+    if(localStorage.getItem('tasks')=== null){
+        tasks=[];
+    }
+    else{
+        tasks=localStorage.getItem('tasks').split(',');
+    }
+	tasks.push(todo,value);
+
+	localStorage.setItem(todo,value);
+    
+};
+
+
